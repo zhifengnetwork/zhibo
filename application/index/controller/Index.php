d6cf2c2420ed7df19350ec2f0aff8dfbd28ce573
FZJ <yudwcom@163.com>
2019-05-06 20:03:38 +0800

    直播

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,8 +3,82 @@ namespace app\index\controller;
 
 class Index
 {
+
+    /**
+     * 析构流函数
+    */
+    public function  __construct() {
+      
+
+       
+    }
+
+    /**
+     * flv格式
+     */
+    public function flv(){
+        $app = 'dc';
+        $user_id = '56895';
+        $data = $this->sign($app,$user_id);
+        echo $data['flv'];
+    }
+
+    /**
+     * 推流
+     */
+    public function tui(){
+        $app = 'dc';
+        $user_id = '56895';
+        $data = $this->sign($app,$user_id);
+        echo $data['tui'];
+    }
+
+    /**
+     * 首页
+     */
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+        $app = 'dc';
+        $user_id = '56895';
+        $data = $this->sign($app,$user_id);
+        dump($data);
     }
+
+
+    /**
+     * 生成签名
+     */
+    private function sign($app,$user_id)
+    {
+        $sercet = '0cN03mTxm3';
+        // $time = time() + 300 * 60 * 60;
+
+        $time = '1557157678';
+
+        //gWsPYhPRVT
+
+        $tui_key = $this->get_auth_key($app,$user_id,'',$time,'gWsPYhPRVT');
+        $data['tui'] = 'rtmp://push.zhifengwangluo.com/'.$app.'/user'.$user_id.'?auth_key='.$tui_key;
+
+        $rtmp_key = $this->get_auth_key($app,$user_id,'',$time,$sercet);     
+        $data['rtmp'] = 'rtmp://play.zhifengwangluo.com/'.$app.'/user'.$user_id.'?auth_key='.$rtmp_key;
+
+        $flv_key = $this->get_auth_key($app,$user_id,'.flv',$time,$sercet);
+        $data['flv'] = 'http://play.zhifengwangluo.com/'.$app.'/user'.$user_id.'.flv?auth_key='.$flv_key;
+
+        $m3u8_key = $this->get_auth_key($app,$user_id,'.m3u8',$time,$sercet);
+        $data['m3u8'] = 'http://play.zhifengwangluo.com/'.$app.'/user'.$user_id.'.m3u8?auth_key='.$m3u8_key;
+
+       return $data;        
+    }
+
+    private function get_auth_key($app,$user_id,$ext,$time,$sercet){
+
+        $md5 = "/".$app."/user".$user_id.$ext."-".$time."-0-0-".$sercet;
+        $md5 = md5($md5);
+        $key = $time.'-0-0-'.$md5;
+
+        return $key;
+    }
+
 }
